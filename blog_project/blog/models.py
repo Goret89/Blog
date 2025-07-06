164970eedc932f4e8e0c2ead8b6b9661114c27ff
Kateryna Horeta <kateryna.horeta@calyx.ai>
2025-07-06 18:57:11 +0300

    Added markdown

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import markdown
 from django.contrib.auth.models import User
+from django.utils.safestring import mark_safe
 
 # Create your models here.
 class Post(models.Model):
@@ -13,10 +14,14 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     def get_markdown(self):
-        return markdown.markdown(self.content)
+        return mark_safe(markdown.markdown(self.content))
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_markdown(self):
+        return mark_safe(markdown.markdown(self.content))
