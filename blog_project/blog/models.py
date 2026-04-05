74c81ac2fa69fff6611644e5b94665cca9f2b02c
Kateryna Horeta <kateryna.h@billline.net>
2026-04-05 17:43:37 +0300

    Add likes with AJAX

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -9,12 +9,19 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
 
     def __str__(self):
         return self.title
+
     def get_markdown(self):
         return mark_safe(markdown.markdown(self.content))
+
+    def likes_count(self):
+        return self.votes.filter(value=1).count()
+
+    def dislikes_count(self):
+        return self.votes.filter(value=-1).count()
+
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
@@ -25,3 +32,21 @@ class Comment(models.Model):
     @property
     def get_markdown(self):
         return mark_safe(markdown.markdown(self.content))
+
+
+class Vote(models.Model):
+    LIKE = 1
+    DISLIKE = -1
+
+    VALUE_CHOICES = (
+        (LIKE, 'Like'),
+        (DISLIKE, 'Dislike'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='votes', on_delete=models.CASCADE)
+    value = models.SmallIntegerField(choices=VALUE_CHOICES)
+
+    class Meta:
+        unique_together = ('user', 'post') # duplicate protection
+
